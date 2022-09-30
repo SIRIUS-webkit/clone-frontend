343e3b3e01c2919ab392bbdd1ad19371acef3cd9
La Min Ko Ko <69799049+SIRIUS-webkit@users.noreply.github.com>
2022-09-30 20:19:00 +0630

    first setup

---
+++ function/callService.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+import axios from "axios";
+
+export default async function callService(method, url, data, headers) {
+  const response = await axios({
+    method,
+    headers,
+    url,
+    data,
+  })
+    .then((res) => {
+      return res;
+    })
+    .catch((error) => {
+      if (error.response) {
+        if (
+          error.response.data.error === undefined ||
+          error.response.data.error === "" ||
+          error.response.data.error === null
+        ) {
+          return "server error";
+        }
+
+        return error.response.data.error;
+      }
+      if (error.request) {
+        return "request error";
+      }
+
+      return "has something error";
+    });
+
+  return response;
+}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+require("dotenv").config({ path: `${process.env.NODE_ENV}` });
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  env: {
+    domainAPI: process.env.DOMAIN_API,
+  },
+  webpack: (config, isServer) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
